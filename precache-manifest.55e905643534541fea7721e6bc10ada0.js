a4c101b20b8d45b887dc3704f03bc178b42bdc58
Rodion <dekoublack@gmail.com>
2019-03-03 17:18:32 +0200

    first commit

---
+++ precache-manifest.55e905643534541fea7721e6bc10ada0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e204140d4e413c39554",
+    "url": "/static/css/main.dbee429c.chunk.css"
+  },
+  {
+    "revision": "2e204140d4e413c39554",
+    "url": "/static/js/main.2e204140.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cc3caa51dc36eafb4c3e",
+    "url": "/static/js/2.cc3caa51.chunk.js"
+  },
+  {
+    "revision": "29a0be9fd39fe50b6ce71459b64abac8",
+    "url": "/index.html"
+  }
+];
