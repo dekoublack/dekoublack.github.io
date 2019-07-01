2a549b80bf536f234edc74f3d07b64b2218c686c
Rodion <dekoublack@gmail.com>
2019-07-01 19:58:45 +0300

    Recipes finder

---
+++ precache-manifest.3600cfbb8411308ddfde68eb1bbb09ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dff365de55990ea4417d",
+    "url": "/static/css/main.221bcbc9.chunk.css"
+  },
+  {
+    "revision": "dff365de55990ea4417d",
+    "url": "/static/js/main.dff365de.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a3e53b2f57f7cbf49e53",
+    "url": "/static/js/2.a3e53b2f.chunk.js"
+  },
+  {
+    "revision": "fc14175fa16e376536b2214e60d5333a",
+    "url": "/index.html"
+  }
+];
